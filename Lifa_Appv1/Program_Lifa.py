638b23f13db575c8b9c6c0348e210063052e4500
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2021-11-07 07:59:11 -0800

    Here ya go

---
+++ Lifa_Appv1/Program_Lifa.py
@@ -0,0 +1,23 @@
+# (Lifa App - Program) - Version 1.0 by Flames Co. Software 2021
+#
+## Load the Lifa App
+#!/usr/bin/env python3
+# Path: Program_Lifa.py
+## Lifa Quantum Browser
+print("Welcome to the Lifa App")
+print("Please enter your name:")
+name = input()
+print("Hello " + name + "!")
+print("Please enter your age:")
+age = input()
+print("You will be " + str(int(age) + 1) + " in a year.")
+print("Please enter your height in cm:")
+height = input()
+print("You are " + str(int(height) * 2.54) + " inches tall.")
+print("Please enter your weight in kg:")
+weight = input()
+print("You are " + str(int(weight) * 2.20) + " pounds heavy.")
+print("Please enter your desired  lifestyle:")
+lifestyle = input()
+print("You will be " + lifestyle + " in a day.")
+print("Thank you for using the Lifa App!")
